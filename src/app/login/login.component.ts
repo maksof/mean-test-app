e9abcd7be8b9de67aef278f4a7678e681eab823c
Hussain <mohammadhussainmobeen@gmail.com>
2019-03-11 15:18:12 +0500

    login and sign up api integrations

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import {SharedService} from '../shared.service';
+import {CommonService} from '../common.service';
+import { Router } from '@angular/router'
+import {NotificationsService, SimpleNotificationsModule } from 'angular2-notifications';
+
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
 	loginObj:any = {};
 
-  constructor() { }
+	showLoader:boolean = false;
+	constructor(public sharedService:SharedService, public notificationService:NotificationsService, public commonService:CommonService, public route:Router) {
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+	loginUser(){
+		if (this.commonService.required(this.loginObj.email) && this.commonService.required(this.loginObj.password)) {
+			if (this.commonService.checkValidEmail(this.loginObj.email)) {
+				this.sharedService.login(this.loginObj).subscribe(res=>{
+					this.showLoader = false;
+					if (res.status == "OK") {
+						var user = "";
+						user = JSON.stringify(res.data);
+						localStorage.setItem('user',user);
+						if (res.data.role == "ADMIN") {
+							this.route.navigateByUrl("admin");
+						}else if(res.data.role == "USER"){
+							this.route.navigateByUrl("dashboard");
+						}
+						this.loginObj = {};
+						this.notificationService.success("Success", "Successfully login");
+					}else{
+						this.notificationService.info("Info", res.message);
+					}
+				},(error)=>{
+					this.showLoader = false;
+					this.notificationService.error("Error", "Internal Server Error.");
+				});
+			}else{this.notificationService.error('Error','Please enter a valid email address');}
+		}else{this.notificationService.error('Error','Please fill all the required (*) fields.');}
+	}
 
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {SharedService} from '../shared.service';
 import {CommonService} from '../common.service';
+import { Router } from '@angular/router'
 import {NotificationsService, SimpleNotificationsModule } from 'angular2-notifications';
 @Component({
 	selector: 'app-sign-up',
@@ -8,40 +9,42 @@ import {NotificationsService, SimpleNotificationsModule } from 'angular2-notific
 	styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+
+	constructor(public sharedService:SharedService, public notificationService:NotificationsService, public commonService:CommonService, public router:Router) { }
+
 	signupObj:any = {};
-
-	constructor(public sharedService:SharedService, public notificationService:NotificationsService, public commonService:CommonService) { }
-
 	public data : any;
+	showLoader:boolean = false;
 
 	ngOnInit() {
 	}
 	registerUser(){
-		 if (this.commonService.required(this.signupObj.firstName) && this.commonService.required(this.signupObj.lastName) && this.commonService.required(this.signupObj.Email) && this.commonService.required(this.signupObj.username) && this.commonService.required(this.signupObj.password) && this.commonService.required(this.signupObj.confirmPassword)) {
-		
-		 	if(this.commonService.checkValidEmail(this.signupObj.Email)){
-				
+		if (this.commonService.required(this.signupObj.firstName) && this.commonService.required(this.signupObj.lastName) && this.commonService.required(this.signupObj.Email) && this.commonService.required(this.signupObj.age) && this.commonService.required(this.signupObj.password) && this.commonService.required(this.signupObj.confirmPassword)&& this.commonService.required(this.signupObj.gender)) {
+			if(this.commonService.checkValidEmail(this.signupObj.Email)){
 				if(this.commonService.checkValidMobileNumber(this.signupObj.mobileNumber)) {
-
-					if (this.signupObj.password != this.signupObj.confirmPassword) {
-						this.notificationService.error('Error','Password not matched');
-					}else{
-						this.sharedService.registerUser(this.signupObj).subscribe(res =>{
-						this.data = res.data;
-						this.notificationService.success("Success", "Successfully login");
-						},(error)=>{
-						this.notificationService.error("Error", "Internal Server Error.");
-						})
-					}
-					
-				}else{
-					this.notificationService.error('Error','Please enter a valid mobile number of 10 digits');
-				}
-			}else{
-		 		this.notificationService.error('Error','Please enter a valid email address');
-			}
-		 }else{
-		 	this.notificationService.error('Error','Please fill all the required (*) fields.');
-		}
+					if (this.checkValidAge(this.signupObj.age)) {
+						if (this.signupObj.password != this.signupObj.confirmPassword) {
+							this.notificationService.error('Error','Password not matched');
+						}else{
+							this.showLoader = true;
+							this.sharedService.registerUser(this.signupObj).subscribe(res =>{
+								this.showLoader = false;
+								if (res.status == "OK") {
+									this.signupObj = {};
+									this.notificationService.success("Success", "Successfully Registered.");
+								}
+							},(error)=>{
+								this.showLoader = false;
+								this.notificationService.error("Error", "Internal Server Error.");
+							});
+						}
+					}else{this.notificationService.error('Error',"Please Enter a Valid Year")}
+				}else{this.notificationService.error('Error','Please enter a valid mobile number of 10 digits');}
+			}else{this.notificationService.error('Error','Please enter a valid email address');}
+		 }else{this.notificationService.error('Error','Please fill all the required (*) fields.');}
+	}
+	checkValidAge(age){
+		if (age > 100 || age <= 0) return false;
+		else return true;
 	}
 }
