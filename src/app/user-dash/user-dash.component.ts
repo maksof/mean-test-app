1ef76a6e9f41a9cb482b3c9c9dab91ba81adcd48
Hussain <mohammadhussainmobeen@gmail.com>
2019-03-15 18:36:57 +0500

    user dashboard

--- src/app/user-dash/user-dash.component.ts
+++ src/app/user-dash/user-dash.component.ts
@@ -10,9 +10,8 @@ import {NotificationsService, SimpleNotificationsModule } from 'angular2-notific
 export class UserDashComponent implements OnInit {
 	constructor(public sharedService:SharedService, public commonService:CommonService, public notificationService:NotificationsService) { }
 	// bar chart Varibales
-	public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-		scaleShowVerticalLines: false;
-	public lineChartOptions:any = {
+	public barChartOptions:any = {
+		scaleShowVerticalLines: false,
 		responsive: true,
 		legend: false,
 		scales: {
@@ -26,36 +25,17 @@ export class UserDashComponent implements OnInit {
 					color: "rgba(210, 210, 210, 0.75)",
 				}   
 			}]
-		}, line : { 
-			tension : 0 
-		} 
+		},
 	};
-	public chartColors: Array<any> = [
-	{ // first color
-		backgroundColor: 'rgba(225,10,24,0.2)',
-		borderColor: 'rgba(225,10,24,0.2)',
-		pointBackgroundColor: 'rgba(225,10,24,0.2)',
-		pointBorderColor: '#fff',
-		pointHoverBackgroundColor: '#fff',
-		pointHoverBorderColor: 'rgba(225,10,24,0.2)'
-	}	];
-	public lineChartType:string = 'line';
-	public lineChartLegend:boolean  = true;
-	 // lineChart
-	public lineChartData:Array<any> = [{data: [6500, 5900, 8000, 8100, 5600, 5500, 4000, 900, 9500 , 10005, 5400, 6500], label: 'Ranking'}];
-	// pie chart Varibales
-	public pieChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
-	public pieChartData:number[] = [300, 500, 100];
-	public pieChartType:string = 'pie';
-
-	// doghnut chart Varibales
-	public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-	public doughnutChartData:number[] = [350, 450, 100];
-	public doughnutChartType:string = 'doughnut';
+	public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+	public barChartType:string = 'bar';
+	public barChartLegend:boolean = true;
+	public barChartData: any[] = [{data: [20,10,50,60,60,20,11], label: 'Quiz', backgroundColor: ['#163293',]}];
 
 	mainToggle:boolean = true;
 	showLoader:boolean = false;
 	timePeriod:any = [];
+	timeId = '';
 	ngOnInit() {
 		this.getAllTimePeriod();
 	}
@@ -69,17 +49,21 @@ export class UserDashComponent implements OnInit {
 		this.sharedService.getAllTimePeriod().subscribe(res=>{
 			this.showLoader = false;
 			this.timePeriod = res.data;
+			this.timeId = this.timePeriod[0].id;
+			console.log(this.timeId,this.timePeriod);
 		},(error)=>{
 			this.showLoader = false;
 			this.notificationService.error("Error!","Internal Server Error.");
 		})
 	}
-	getMoviesOnTimePeriod(id){
+	
+	getTimePeriodDashBoarData(){
 		this.showLoader = true;
-		this.sharedService.getMoviesOnTimePeriod(id).subscribe(res=>{
+		this.sharedService.getMoviesOnTimePeriod(this.timeId).subscribe(res=>{
+			console.log(res);
 			this.showLoader = false;
 		},(error)=>{
 			this.showLoader = false;
-		})
+		});
 	}
 }
