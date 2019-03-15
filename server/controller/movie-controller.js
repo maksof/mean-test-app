d01badea79a366f2a22065ff636ccadb1c6bcdd7
maksof-Rehan <31398564+maksof-Rehan@users.noreply.github.com>
2019-03-15 10:45:56 +0500

    set movies

--- server/controller/movie-controller.js
+++ server/controller/movie-controller.js
@@ -517,25 +517,25 @@ exports.getMovies = function (request, response) {
 	    var title = request.query.title ? request.query.title : '';
 	    if(categoryId) whereClause.categoryId = categoryId;
 	    if(title) whereClause.title = { $like: title };
-	    tbl_grades.belongsTo(tbl_movies, {foreignKey:'movieId'});
-	    var query = { where : {'userId' : userId},
-	        include : [{
-	            model : tbl_movies,
-	            attributes : ['categoryId','title','year','director','distribution','description','photoUrl'],
-	            where : {'isDeleted': 0, 'isApproved': 1}
-	        }
-	        ]
+	    tbl_movies.belongsTo(tbl_grades, {foreignKey:'id',targetKey : 'movieId'});
+	    var query = { 
+            attributes : ['categoryId','title','year','director','distribution','description','photoUrl'],
+            where : {'isDeleted': 0, 'isApproved': 1},
+            include : [{
+                required : false,
+                model : tbl_grades,
+                where : {'userId' : userId}
+	        }]
 	    }
-	    tbl_grades.findAll(query)
+	    tbl_movies.findAll(query)
 	    .then(function(movies) {
-	        
 	        common.sendResponseBack(response,'OK','data fetched successfully',movies)
 	    }, (error) => {
 	        console.log("error",error)
 	        common.sendResponseBack(response, 'FAIL', 'Some error occured while processing your request, Please try again later.', null);
 	        logger.error( 'Error occured on '+new Date()+' with reason' + error);
 	    });
-    }  else common.sendResponseBack(response, 'FAIL', 'Please pass the user id.', null);
+    }  else common.sendResponseBack(response, 'FAIL', 'Please pass the userId.', null);
 }
 
 /**
@@ -628,6 +628,7 @@ exports.viewGradeMovies = function(request,response) {
  * @apiSuccess {string} status Status of the request.
  * @apiSuccess {string} message Message corresponding to request.
 */
+
 exports.movieWithTimePeriodBasis = function(req,res){
     if(!req.query.years) return res.status(200).json({success : false ,"msg" : "years is missing"})
     var year = req.query.years.split('-');
@@ -644,3 +645,4 @@ exports.movieWithTimePeriodBasis = function(req,res){
         res.status(200).json({"movies" : rangeMovies ,"msg" : "fetched successfully"})
     })
 }
+
